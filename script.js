ef58c7ee2c1e36d2c31d9f1d641f61c39a045dc8
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2023-08-12 03:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let mp = {};
+var newarr = []
+
+touristSpots.forEach((str) => {
+	let ans = "";
+	let arr = str.split(" ");
+	for(let key of arr){
+		let curr = key.toLowerCase();
+		if(curr != "the" && curr != "a" && curr != "an"){
+            ans += key + " ";
+        }
+	}
+    mp[ans] = str;
+    newarr.push(ans);
+});
+
+newarr.sort();
+for (let i = 0; i < newarr.length; i++) {
+    let str = mp[newarr[i]];
+    touristSpots[i] = str;
+}
+
+console.log(touristSpots);
